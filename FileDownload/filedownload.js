eb544f52f6575b87d83bf851d65d207bb62c2721
rajnish <sinharajnish03@gmail.com>
2023-11-26 12:15:58 +0530

    first commit

---
+++ FileDownload/filedownload.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded',()=>{
+    document.getElementById('btn1').addEventListener('click', createBlob);
+});
+
+const createBlob= ()=>{
+    const str= 'Hi goodmorning'
+    let b = new Blob([str],{type:'plain/text'});
+    console.log('blob ',b);
+    let url = URL.createObjectURL(b);
+    // creating anchor tag
+    let a= document.createElement('a');
+    a.href=url;
+    a.download='greetings.txt';
+    // this download property starts browser to local disk download, 
+    // as well as setting default name
+
+    a.textContent='download greetings';
+    document.querySelector('main').append(a); 
+
+   // in case we want it to be downloaded on click
+    // a.href=URL.createObjectURL(b);
+    // a.download='greeting.txt',
+    // a.click();
+
+
+
+
+}
